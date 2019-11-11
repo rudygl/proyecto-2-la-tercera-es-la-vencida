5f9ca78ea369044acb849b9b4d1d55365d11ead6
Dydrey <57191864+Dydrey@users.noreply.github.com>
2019-11-11 12:12:13 -0600

    Update simulated_annealing.py
    Se creo simmulated anneling en base al algoritmo del documento de optimizacion

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -1,17 +1,35 @@
-def optimizar(dominio, temperatura = 10e32, tasa_enfriamiento = 0.95):
-    """Algoritmo de optimización estocástica simulated annealing.
+import math
 
+def optimizar(dominio, temperatura = 10e32, tasa_enfriamiento = 0.95):
+
+    
+    solucionA = dominio.generar()
+    costoA = dominio.fcosto(solucionA)
+
+    while (temperatura > 0.01):
+
+        solucionB = dominio.vecino(solucionA)
+        costoB = dominio.fcosto(solucionB)
+        p = math.exp(-abs(costoB - costoA)/temperatura)
+        pAzar = random.randint(0, 1)
+
+        if (costoB < costoA) or pAzar <= p:
+            costoA = costoB
+            solucionA = solucionB
+
+    temperatura = temperatura * tasa_enfriamiento
+
+    return solucionA
+
+    """Algoritmo de optimización estocástica simulated annealing.
     Entradas:
     dominio (Dominio)
         Un objeto que modela el dominio del problema que se quiere aproximar.
-
     temperatura (float/int)
         Temperatura inicial del algoritmo, se recomienda un número alto
-
     tasa_enfriamiento (float)
         Porcentaje de enfriamiento de la temperatura durante cada iteración, el valor
         por defecto es 0.95, lo que indica una tasa de enfriamiento del 5%.
-
     Salidas:
         (estructura de datos) Estructura de datos según el dominio, que representa una
         aproximación a la mejor solución al problema.
